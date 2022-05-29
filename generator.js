cbcd991e6701258a39a42abb6423ef790838efda
firehist <benjamin.longearet@elium.io>
2022-05-29 21:08:06 +0200

    New signature

--- generator.js
+++ generator.js
@@ -27,10 +27,13 @@ const signature = () => {
     return `<table>
         <tbody>
       <tr>
-      <td style="padding-bottom: 20px" colspan="7">
-        <div style="margin-block-start: 0px; font-weight:bold; font-size:18px; color:#267CD9; font-family:Georgia; padding-bottom: 20px;">
-            <img height="40px" src="${url}/assets/logo-ape-beauvoisin-2020.jpg" /> L'équipe APE Maternelle 2020
-        </div>
+        <td style="padding-bottom: 20px">
+          <img height="140px" src="${url}/assets/logo-ape-beauvoisin-2022.jpeg" />
+        </td>
+        <td style="padding-bottom: 20px; padding-left: 20px;" colspan="8">
+          <div style="margin-block-start: 0px; font-weight:bold; font-size:18px; color:#267CD9; font-family:Georgia; padding-bottom: 20px;">
+             L'équipe APE Maternelle 2022
+          </div>
           <div style="font-family: Avenir-Book; font-size: 14px; color: #6A6F7D; letter-spacing: 0.82px; line-height: 26px; padding-bottom: 12px;">
              <img width="22px" height="22px" src="${url}/assets/i_mail.png" /> ape.beauvoisin30@gmail.com <br/>
             <a href="https://www.facebook.com/apebeauvoisin" target="_blank" style="text-decoration: none;color: #6A6F7D;line-height: 22px;display: inline-block;">
@@ -63,6 +66,9 @@ const signature = () => {
         </td>
         <td style="padding: 0 5px;" valign="top" align="center" width="60px">
           <img width="60px" height="60px" src="${url}/assets/avatar_vanessa.png" />
+        </td>
+        <td style="padding: 0 5px;" valign="top" align="center" width="60px">
+          <img width="60px" height="60px" src="${url}/assets/avatar_cindy.png" />
         </td>
       </tr>
       <tr>
@@ -104,6 +110,11 @@ const signature = () => {
         <td style="padding: 0 20px;" valign="top" align="center" width="90px">
           <div style="margin-block-start: 0px; font-weight:bold; font-size:16px; color:#267CD9; font-family:Georgia;">
             Vanessa
+          </div>
+        </td>
+        <td style="padding: 0 20px;" valign="top" align="center" width="90px">
+          <div style="margin-block-start: 0px; font-weight:bold; font-size:16px; color:#267CD9; font-family:Georgia;">
+            Cindy
           </div>
         </td>
       </tr>
